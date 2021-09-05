67914d99b3671f40edc4ea40d6ac947ba9507835
vadim-mg <vadikm1981@yandex.ru>
2021-09-05 13:35:23 +0500

    feat:preloader

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,9 +1,13 @@
-function Preloader() {
+import './Preloader.css'
+
+const Preloader = () => {
   return (
-    <div className="Preloader">
-      <p>Preloader</p>
-    </div >
-  );
-}
+    <div className="preloader">
+      <div className="preloader__container">
+        <span className="preloader__round"></span>
+      </div>
+    </div>
+  )
+};
 
 export default Preloader

f08f51ba7a3eeb4f0f89de28aa152baa231d545f
vadim-mg <vadikm1981@yandex.ru>
2021-09-05 13:34:30 +0500

    feat: searchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,8 +1,14 @@
+import "./SearchForm.css"
+import CheckBox from "../CheckBox/CheckBox";
+import Container from "../Container/Container";
+
 function SearchForm() {
   return (
-    <div className="SearchForm">
-      <p>SearchForm</p>
-    </div >
+    <Container additionalContainerClass="container_size_lg search-form">
+      <input className="search-form__field" placeholder="Фильм"  type="text" />
+      <button className="search-form__search-button">Найти</button>
+      <CheckBox className="search-form__filter" name="filter">Короткометражки</CheckBox>
+    </Container >
   );
 }
 

cea3460dccc76cfb132f033eb194e50eb5efad3b
vadim-mg <vadikm1981@yandex.ru>
2021-09-10 14:33:00 +0500

    fix: gitconfig not ignorecase

---
+++ src/components/App/App.js
@@ -0,0 +1,59 @@
+import './App.css'
+import Main from '../Main/Main'
+import Movies from '../Movies/Movies'
+import SavedMovies from '../SavedMovies/SavedMovies'
+import Profile from '../Profile/Profile'
+import Register from '../Register/Register'
+import Login from '../Login/Login'
+import Page404 from '../Page404/Page404'
+import { Route, Switch } from 'react-router-dom'
+import { CurrentUserContext } from '../../contexts/CurrentUserContext'
+import { useState } from 'react'
+
+function App() {
+
+  // на этапе верстки, ставим по дефолту true чтоб увидеть интерфейс авторизованного пользователя
+  // eslint-disable-next-line no-unused-vars
+  const [currentUser, setCurrentUser] = useState({
+    loggedIn: true,
+    name: "Виталий",
+    email: "pochta@yandex.ru"
+  });
+
+
+
+
+  return (
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="app">
+        <Switch>
+          <Route exact path="/">
+            <Main />
+          </Route>
+          <Route path="/movies">
+            <Movies />
+          </Route>
+          <Route path="/saved-movies">
+            <SavedMovies />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+
+
+          <Route path="/signup">
+            <Register />
+          </Route>
+          <Route path="/signin">
+            <Login />
+          </Route>
+          <Route>
+            <Page404 />
+          </Route>
+        </Switch>
+      </div >
+    </CurrentUserContext.Provider >
+  );
+}
+
+export default App;
